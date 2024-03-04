50cce657dc94be625fe96a7399d2c5402fd91dd2
Nikhil-Ladha <nikhilladha1999@gmail.com>
2024-03-04 13:44:02 +0530

    build: added new rook bundle creation script
    Added Dockerfile to create rook-ceph-operator-bundle under the rook repo.
    Updated the Makefile to have the 'bundle' command, that reference the 'gen-bundle.sh'
    script under `build/bundle`.
    Moved the rook csv under build/csv/ceph directory to keep all the
    manifests at one location for bundle creation
    
    Signed-off-by: Nikhil-Ladha <nikhilladha1999@gmail.com>

--- build/common.sh
+++ build/common.sh
@@ -24,6 +24,15 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export OUTPUT_DIR=${BUILD_ROOT}/_output
 export WORK_DIR=${BUILD_ROOT}/.work
 export CACHE_DIR=${BUILD_ROOT}/.cache
+export GOOS
+GOOS=$(go env GOOS)
+export GOARCH
+GOARCH=$(go env GOARCH)
+DEFAULT_CSV_VERSION="4.15.0"
+CSV_VERSION="${CSV_VERSION:-${DEFAULT_CSV_VERSION}}"
+export ROOK_IMAGE="docker.io/rook/ceph:v1.13.0.399.g9c0d795e2"
+DEFAULT_BUNDLE_IMAGE=rook/rook-ceph-operator-bundle:"${VERSION}"
+BUNDLE_IMAGE="${BUNDLE_IMAGE:-${DEFAULT_BUNDLE_IMAGE}}"
 
 function ver() {
     local full_ver maj min bug build
